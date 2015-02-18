74e859b470366fb2f3166cf6b080b52dfced4193
shyams <shyams@hsh-global.com>
2015-02-18 15:34:02 +0200

    Created a event issuedetail
    Created a event issuedetail with `async.js` to get the data from view issues, data.js to prepare data,` mustache` to display, `path.txt` to identify the path, and `after.js` to display the map with a pointer. close #11

---
+++ evently/reportandfix/issuedetail/data.js
@@ -0,0 +1,44 @@
+function (data, e, params) {
+  var rows = data.rows;
+  var viewdata = {
+    "issueDetails": []
+  };
+  for (var i = 0; i < rows.length; i++) {
+    var row = rows[i];
+    var doc = row.doc;
+    var image;
+
+    if (doc._attachments) {
+      var imageName = 'issue-' + doc.number;
+      var attachments = Object.keys(doc._attachments);
+
+      for (var imageKey in attachments ) {
+        if (attachments[imageKey].indexOf(imageName) > -1) {
+          image = attachments[imageKey];
+        }
+      };
+    }
+    
+    viewdata.issueDetails.push({
+      "description": doc.desc,
+      "usr_name": doc.usr_name,
+      "issue_id": doc.issue_id,
+      "lat": doc.lat,
+      "lon": doc.lng,
+      "issue_number": doc.number,
+      "status": (doc.closed_at) ? "closed" : "in progress",
+      "image": (doc._attachments)? doc._id + '/' + image : undefined
+      }) 
+  };
+  $(this).trigger('rendernav', [{ 
+    "left": {
+      "icon": "fa-chevron-left",
+      "href": "#/issuelist" 
+    },
+    "title": "Issues",
+    "right": {
+      "icon": "fa-user"
+    } 
+  }]);
+  return viewdata;
+}
